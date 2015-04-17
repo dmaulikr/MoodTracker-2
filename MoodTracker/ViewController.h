ac9264020c6cfbe87b2c73245e2c2e1f5f84667f
swerty <slw247@cornell.edu>
2015-04-17 00:15:59 -0400

    initial commit

---
+++ MoodTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MoodTracker
+//
+//  Created by Sean Wertheim on 4/16/15.
+//  Copyright (c) 2015 Sean Wertheim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
